0d3d685ec202001669ab2328b47011797735985a
Fraudik <52631426+Fraudik@users.noreply.github.com>
2020-03-16 16:50:45 +0200

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -5,16 +5,13 @@ from data import users_resource
 from data import jobs_resource
 app = Flask(__name__)
 api = Api(app)
-
-
-def main():
-    db_session.global_init("db/blogs.sqlite")
-    api.add_resource(users_resource.UsersListResource, '/api/v2/users')
-    api.add_resource(users_resource.UsersResource, '/api/v2/users/<int:user_id>')
-    api.add_resource(jobs_resource.JobsListResource, '/api/v2/jobs')
-    api.add_resource(jobs_resource.JobsResource, '/api/v2/jobs/<int:job_id>')
-    app.run(port=8080, host='127.0.0.1')
-
+db_session.global_init("db/blogs.sqlite")
+api.add_resource(users_resource.UsersListResource, '/api/v2/users')
+api.add_resource(users_resource.UsersResource, '/api/v2/users/<int:user_id>')
+api.add_resource(jobs_resource.JobsListResource, '/api/v2/jobs')
+api.add_resource(jobs_resource.JobsResource, '/api/v2/jobs/<int:job_id>')
+url = 'http://127.0.0.1:00/'
 
 if __name__ == '__main__':
-    main()
+    
+    app.run()
